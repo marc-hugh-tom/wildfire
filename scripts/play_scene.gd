b331c48b77c4ad63d4f5bc5a7e97e716fdb59586
Marc Jones <m4rcj0n35@gmail.com>
2020-07-11 12:08:34 +0200

    Started to implement UI

--- scripts/play_scene.gd
+++ scripts/play_scene.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+signal quit
+
 const debug_level = preload("res://nodes/main.tscn")
 
 func _ready():
-	var level = debug_level.instance()
-	add_child(level)
+	connect_ui_buttons()
+	add_level(debug_level)
+
+func connect_ui_buttons():
+	$hud.get_node("background/buttons/menu").connect(
+		"button_up", self, "emit_signal", ["quit"])
+
+func add_level(level):
+	var unpacked_level = level.instance()
+	add_child(unpacked_level)
+	move_child(unpacked_level, 0)
