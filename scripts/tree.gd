8ffbcb4dad4f3733a6dd6e8ccd1a08aad5b7dd24
Marc Jones <m4rcj0n35@gmail.com>
2020-07-11 09:07:00 +0200

    Separate spread and on_fire sprites for trees

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 onready var fsm = $fire_state_machine
 
-onready var on_fire_texture = preload("res://assets/tree_fire_full.png")
+onready var on_fire_start_texture = preload("res://assets/tree_fire_start.png")
+onready var on_fire_spread_texture = preload("res://assets/tree_fire_full.png")
 
 var pos
 
 func _ready():
 	fsm.connect("on_fire", self, "_on_fire")
+	fsm.connect("spread", self, "_on_spread")
 
 func init(coord):
 	pos = coord
 
 func _on_fire():
-	$Sprite.set_texture(on_fire_texture)
-	print("tree ", pos, " is on fire!")
+	$Sprite.set_texture(on_fire_start_texture)
+
+func _on_spread():
+	$Sprite.set_texture(on_fire_spread_texture)
