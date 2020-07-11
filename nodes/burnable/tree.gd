11108f9a3f02c35ef41e1285ab635bdc55021c09
Tom Craven <tom.cass.craven@gmail.com>
2020-07-11 14:18:22 +0100

    burnable

--- nodes/burnable/tree.gd
+++ nodes/burnable/tree.gd
@@ -1,38 +1,10 @@
-extends Node2D
+extends "res://scripts/burnable.gd"
 
-const FLAME = preload("res://nodes/burnable/flame.tscn")
-
-var heat = 0
-var flash_point = 3
-
-func _ready():
-	$Area2D.connect("area_entered", self, "_on_area_entered")
+func get_initial_heat():
+	return 0
 	
-	$BurnTimer.connect("timeout", self, "_on_BurnTimer_timeout") 
-	$BurnTimer.start()
-
-func _on_area_entered(entity):	
-	var collision_layer = entity.get_collision_layer()
-	var layer_name = ProjectSettings.get_setting(
-		str("layer_names/2d_physics/layer_", collision_layer))
-
-	if layer_name == "flame":
-		heat += 1
-		
-func _on_BurnTimer_timeout():
-	if heat > flash_point:
-		var directions = [
-			Vector2(-1, 0),
-			Vector2(0, 1),
-			Vector2(1, 0),
-			Vector2(0, -1),
-			Vector2(-1, -1),
-			Vector2(1, 1),
-			Vector2(1, -1),
-			Vector2(-1, 1)
-		]
-
-		for direction in directions:
-			var flame = FLAME.instance()
-			flame.set_direction(direction)
-			add_child(flame)
+func get_initial_flash_point():
+	return 3
+	
+func get_initial_fuel():
+	return 5
---
+++ scripts/burnable.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+const FLAME = preload("res://nodes/burnable/flame.tscn")
+
+var heat
+var flash_point
+var fuel
+
+func _ready():
+	$Area2D.connect("area_entered", self, "_on_area_entered")
+
+	$BurnTimer.connect("timeout", self, "_on_BurnTimer_timeout") 
+	$BurnTimer.start()
+	
+	heat = get_initial_heat()
+	flash_point = get_initial_flash_point()
+	fuel = get_initial_fuel()
+
+func get_initial_heat():
+	return 0
+	
+func get_initial_flash_point():
+	return 3
+	
+func get_initial_fuel():
+	return 5
+
+func _on_area_entered(entity):
+	if is_a_parent_of(entity):
+		return
+
+	var collision_layer = entity.get_collision_layer()
+	var layer_name = ProjectSettings.get_setting(
+		str("layer_names/2d_physics/layer_", collision_layer))
+
+	if layer_name == "flame":
+		heat += 1
+		
+func _on_BurnTimer_timeout():
+	if heat > flash_point and fuel > 0:
+		var directions = [
+			Vector2(-1, 0),
+			Vector2(0, 1),
+			Vector2(1, 0),
+			Vector2(0, -1),
+			Vector2(-1, -1),
+			Vector2(1, 1),
+			Vector2(1, -1),
+			Vector2(-1, 1)
+		]
+
+		for direction in directions:
+			var flame = FLAME.instance()
+			flame.set_direction(direction)
+			add_child(flame)
+
+		fuel -= 1
+	elif fuel <= 0:
+		# change sprite
+		pass
