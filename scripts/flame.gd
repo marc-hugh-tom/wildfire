e276a1f973c5ff9e94bb5fda620483551a47bb8d
Tom Craven <tom.cass.craven@gmail.com>
2020-07-11 19:01:15 +0100

    ice block wip

--- scripts/flame.gd
+++ scripts/flame.gd
@@ -4,6 +4,9 @@ var speed = 3
 var direction
 var target_position
 
+func _ready():
+	$Area2D.connect("area_entered", self, "_on_area_entered")
+
 func set_direction(d):
 	direction = d
 	target_position = d * 32
@@ -11,4 +14,12 @@ func set_direction(d):
 func _process(delta):
 	position = lerp(position, target_position, delta * speed)
 	if position.distance_to(target_position) < 1:
+		queue_free()
+
+func _on_area_entered(entity):
+	var collision_layer = entity.get_collision_layer()
+	var layer_name = ProjectSettings.get_setting(
+		str("layer_names/2d_physics/layer_", collision_layer))
+
+	if layer_name == "ice":
 		queue_free()
