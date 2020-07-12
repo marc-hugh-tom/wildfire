35520085e74b8fb91c8a9d7be88e2cc18bcb963d
Tom Craven <tom.cass.craven@gmail.com>
2020-07-12 11:40:52 +0100

    add thank you level

---
+++ scripts/thank_you.gd
@@ -0,0 +1,13 @@
+extends "res://scripts/base_level.gd"
+
+func get_start_money():
+	return 99
+
+func get_available_items():
+	return []
+
+func get_start_time_seconds():
+	return 99
+
+func get_next_level():
+	return null
