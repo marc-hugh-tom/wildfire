a38f1ff684a33eaf8d951e073b7b31c7ed869023
Tom Craven <tom.cass.craven@gmail.com>
2020-07-12 17:39:36 +0100

    level 7

---
+++ levels/level_7.gd
@@ -0,0 +1,18 @@
+extends "res://scripts/base_level.gd"
+
+func get_start_money():
+	return 12
+
+func get_available_items():
+	return [
+		"Ice Cube", "Firework", "Petrol Can"
+	]
+
+func get_start_time_seconds():
+	return 20
+
+func get_next_level():
+	return "level_8"
+
+func get_tutorial_text():
+	return("You're on your own!")
